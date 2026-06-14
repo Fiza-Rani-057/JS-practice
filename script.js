bbea716ed8f1ffcb712976ad646da7d631d19a01
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-14 23:49:54 +0500

    update

--- script.js
+++ script.js
@@ -13,12 +13,33 @@
 //  var(Keyword) , container name ,  = , string 
  
   var  userName = "Fiza";
-   alert(userName);
+   console.log(userName);
   var userEmail = "abcdefg@gmail.com";
   console.log(userEmail);
 
-   // chapter 3
+   // Chapter 3
    // Variable for numbers 
    // Syntax 
-   
+   //   variable 1
+   var num1 = 7;
+   console.log(num1);
+
+   var num2 = 3;
+   console.log(num2);
+
+   var sum = num1 + num2;
+   console.log(sum);
+
+   // Variable 2 
+   var num3 = 2;
+   console.log(num3);
+
+   var num4 = 3;
+   console.log(num4);
+
+   var sum2 =  num3+ 3;
+   console.log(sum2);
+
+
+
 
