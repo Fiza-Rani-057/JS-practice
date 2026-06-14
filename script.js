4798be5a1d773b0dc81932ebbd505b9a35192f2d
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-14 23:32:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//  Chapter 1
+//   Alert 
+//    Syntax 
+//   alert, paranthysis () double qoutataion mark inside [("") () ]
+//    Semicolon after () 
+
+   alert("Hello world");
+   alert("I'm learning Javascript");
+
+//    Chapter 2
+// Variable for names 
+//   Syntax 
+//  var(Keyword) , container name ,  = , string 
+ 
+  var  userName = "Fiza";
+  alert(userName);
